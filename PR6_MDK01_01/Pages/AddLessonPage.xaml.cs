b59f39594d328e509594b63fa515867da8577125
Елизавета Романова <soika-peresmeshnica2015@yandex.ru>
2022-11-02 23:27:54 +0300

    CommitAddLesson

--- PR6_MDK01_01/Pages/AddLessonPage.xaml.cs
+++ PR6_MDK01_01/Pages/AddLessonPage.xaml.cs
@@ -27,11 +27,70 @@ namespace PR6_MDK01_01.Pages
             InitializeComponent();
             teacher = ls;
             tbTeacher.Text = teacher.ShortName;
+            cbGroup.ItemsSource = DataBaseClass.connect.StudyPlan.Where(x => x.IdTeacher == ls.IdTeacher).ToList();
+            cbGroup.SelectedValuePath = "IdGroup";
+            cbGroup.DisplayMemberPath = "Groups.NameGroup";
+            
         }
 
         private void btnBack_Click(object sender, RoutedEventArgs e)
         {
             FrameClass.frmLoad.Navigate(new LessonsPage(teacher));
+        }
+
+
+        bool IsClear(int group, int discipline, int type, string date)
+        {
+            if (group != -1)
+            {
+                if (discipline != -1)
+                {
+                    if (type != -1)
+                    {
+                        if (date != "")
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            MessageBox.Show("Выберите дату", "Добавление занятия", MessageBoxButton.OK, MessageBoxImage.Error);
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Выберите преподавателя из списка", "Добавление занятия", MessageBoxButton.OK, MessageBoxImage.Error);
+                        return false;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Выберите вид отчетности из списка", "Добавление занятия", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return false;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Выберите группу из списка", "Добавление занятия", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+        }
+
+        private void TypeLesson()
+        {
+            
+        }
+
+        private void cbGroup_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(cbGroup.SelectedIndex != -1)
+            {
+                cbDisc.Visibility = Visibility.Visible;
+                cbDisc.ItemsSource = DataBaseClass.connect.StudyPlan.Where(x => x.IdGroup == (int)cbGroup.SelectedValue && (x.Lecture != 0 || x.Practice != 0)).ToList();
+                cbDisc.SelectedValuePath = "IdDiscipline";
+                cbDisc.DisplayMemberPath = "Disciplines.Discipline";
+            }
+            
         }
     }
 }
